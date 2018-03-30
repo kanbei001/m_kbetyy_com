2988168f1452ffdf529a3b32731f9e096e9b4061
funnenglun <kbetyy@126.com>
2018-03-31 04:00:01 +0800

    2018_03_31_04_00_01

--- data/cache/listcache/76/59/a5/7659a56f882bbed2d753196f52785be6.php
+++ data/cache/listcache/76/59/a5/7659a56f882bbed2d753196f52785be6.php
@@ -1,2 +1,2 @@
 <?php exit('dedecms');?>
-a:2:{s:4:"data";a:1:{s:2:"dd";s:2:"10";}s:7:"timeout";i:1522207140;}
+a:2:{s:4:"data";a:1:{s:2:"dd";s:2:"11";}s:7:"timeout";i:1522403349;}
--- templets/default/js/kangbei_order.js
+++ templets/default/js/kangbei_order.js
@@ -1,5 +1,5 @@
 $(document).ready(function(){
-    //科室列表
+    //获取科室
     url = "/plus/API.php";
     postData = {k:2};
     $.post(url, postData, function(result) {
@@ -21,17 +21,13 @@ function hide_order(){
 	$('.order-screen').fadeOut();
 }
 function post_order(){
-	var gh_name = $("input[name='gh_name']");//姓名
-	//var gh_sex = $("input[name='gh_sex']");//性别
-	//var gh_age = $("input[name='gh_age']");//年龄
-	var gh_tel = $("input[name='gh_tel']");//电话
-	//var gh_qq = $("input[name='gh_qq']");//QQ
-	var gh_date = $("input[name='gh_date']");//时间
-	var gh_offices = $('select[name="gh_offices"] option:selected');//科室
-    var gh_des = $("textarea[name='gh_des']");//描述
-	//var gh_code = $("input[name='gh_code']");
-	//var gh_fromurl = window.location.href;//来源
-    var tel_re = /^(13[0-9]|14[5|7]|15[0|1|2|3|5|6|7|8|9]|17[0-9]|18[0|1|2|3|5|6|7|8|9])\d{8}$/;
+	var gh_name = $("#name");//姓名
+	var gh_tel = $("#tel");//电话
+	var gh_date = $("#time");//时间
+	var gh_offices = $("#keshi option:selected");//科室
+	var gh_sex = $("#sex");//性别
+	var gh_des = $("#des");//描述
+	tel_re = /^(1\d{10}|((010|02\d|0[3-9]\d{2})-?)?([1-9]\d{6,7}))$/;
 	if(gh_name.length>0 &&gh_name.val() ==""){
 		showMsg('姓名不能为空！',2000);
 		gh_name.focus();
@@ -46,7 +42,12 @@ function post_order(){
     showMsg('联系电话格式不正确，请输入11位手机号码！',2000);
     gh_tel.focus();
 		return false;
-    }
+  }
+	if(gh_sex.length>0 &&gh_sex.val() =="0"){
+		showMsg('请选择性别！',2000);
+    gh_sex.focus();
+		return false;
+	}
 	if(gh_offices.length>0 &&gh_offices.val() =="0"){
 		showMsg('请选择科室！',2000);
     gh_offices.focus();
@@ -65,38 +66,36 @@ function post_order(){
   data["name"] = gh_name.val();
   data["phone"] = gh_tel.val();
   data["keshi"] = gh_offices.val();
+  data["sex"] = gh_sex.val();
   data["time"]  = gh_date.val();
   data["bingzheng"] = gh_des.val();
-  var jsondata = {name:data["name"],phone:data["phone"],keshi:data["keshi"],time:data['time'],bingzheng:data["bingzheng"],k:data["k"],sourceUrl:data["sourceUrl"]};
+  var jsondata = {name:data["name"],phone:data["phone"],keshi:data["keshi"],time:data['time'],bingzheng:data["bingzheng"],k:data["k"],sourceUrl:data["sourceUrl"],sex:data["sex"]};
   url = '/plus/API.php';
   $.post(url,jsondata, function(result) {
-    try {
       var result = JSON.parse(result);
-    } catch(error) {
-        　　
-    } 
       if(result.status==1) {
         showMsg("预约成功！我们将会在30分钟内联系您。");
         $('.order-screen').fadeOut(300);
       }else{
-        if(result.error_det) {
-            showMsg(result.error_det);
-        }else{
-           showMsg("提交失败，请拨打电话0755-25111120进行预约。");
-        }
+        showMsg("提交失败，请拨打电话0755-25111120进行预约。");
       }
   });
 }
 /*消息提示框，用来代替alert*/
 /*data参数为提示内容，必需*/
 /*time参数为显示时间，到时间后自动消失，可选*/
-function showMsg(data, time){
+/*width参数为窗口宽度，可选*/
+function showMsg(data, time, width){
+  $(".msg").remove();
+  (typeof(timeout1) != "undefined") ? clearTimeout(timeout1) : "";
+  (typeof(timeout2) != "undefined") ? clearTimeout(timeout2) : "";
   var msg = data ? data : "";
-  var msg_style = '<style class="msg_style msg_remove">.msg{display:none; background:#fff; position:fixed; top:35%; left:50%; width:280px; margin-left:-140px; text-align:center; border-radius:5px; color:#555; box-shadow:0 0 10px 2px rgba(0,0,0,0.5); z-index:1100;} .msg .close{position:absolute; top:0; right:0; width:30px; height:30px; line-height:30px; font-size:26px; font-weight:bold; background:rgba(0,0,0,0.1); cursor:pointer;} .msg h3{border-bottom:1px solid #ccc; margin:0 10px; padding:10px 0;} .msg p{margin:15px;}</style>';
+  var msg_width = width ? width : 280;
+  var msg_style = '<style class="msg_style msg_remove">.msg{display:none; background:#fff; position:fixed; top:35%; left:50%; width:'+msg_width+'px; max-width:500px; min-width:280px; text-align:center; border-radius:5px; color:#555; box-shadow:0 0 10px 2px rgba(0,0,0,0.5); z-index:1100;} .msg .close{position:absolute; top:0; right:0; width:30px; height:30px; line-height:30px; font-size:26px; font-weight:bold; background:rgba(0,0,0,0.1); cursor:pointer;} .msg h3{border-bottom:1px solid #ccc; margin:0 10px; padding:10px 0;} .msg p{margin:15px;}</style>';
   var msg_html = '<div class="msg msg_remove"><div class="close">×</div><h3>温馨提示</h3><p>'+msg+'</p></div>';
-  var msg_script = '<script class="msg_script msg_remove">$(".msg .close").click(function(){$(".msg").fadeOut(300);setTimeout(function(){$(".msg_remove").remove()},1000)})<\/script>';
+  var msg_script = '<script class="msg_script msg_remove">\n$(".msg").css("margin-left",-$(".msg").width()/2+"px");\n$(".msg .close").click(function(){$(".msg").fadeOut(300);\nsetTimeout(function(){$(".msg_remove").remove()},1000)})\n<\/script>';
   $("body").append(msg_style+msg_html+msg_script);
   $(".msg").fadeIn(300);
-  var htime_script = time ? '<script class="htime_script msg_remove">setTimeout(function(){$(".msg").fadeOut(300)},'+time+');setTimeout(function(){$(".msg_remove").remove()},'+(time+300)+')<\/script>' : '';
+  var htime_script = time ? '<script class="htime_script msg_remove">var timeout1 = setTimeout(function(){$(".msg").fadeOut(300)},'+time+');var timeout2 = setTimeout(function(){$(".msg_remove").remove()},'+(time+300)+')<\/script>' : '';
   $("body").append(htime_script);
 }
